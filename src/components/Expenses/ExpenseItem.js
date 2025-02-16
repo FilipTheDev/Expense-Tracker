a4b05b53794296ff28612c15a3347d9c1b88a3f6
FilipTheDev <ftomevski16@gmail.com>
2025-02-16 01:23:16 +0100

    Added changing titles using State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // }
   const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  }
+    let newTitle = prompt("Enter new title:");
+    if (newTitle.length > 0) {
+      setTitle(newTitle);
+      console.log(newTitle);
+    } else {
+      alert("Error, try again.");
+    }
+  };
 
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
